ef78000d01be171c5da7e07173f9ce39f800c191
mydevby <mydev.by@gmail.com>
2023-03-14 00:14:11 +0300

    update todo list

---
+++ build/precache-manifest.bdabd8841a8eab9fa802243b2a266f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f59128f66323ad5271",
+    "url": "/static/css/main.40888f22.chunk.css"
+  },
+  {
+    "revision": "c3f59128f66323ad5271",
+    "url": "/static/js/main.c3f59128.chunk.js"
+  },
+  {
+    "revision": "61d470a77cae633d0bb0",
+    "url": "/static/js/1.61d470a7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "141fafb16cb78b2529f0aeb9f9aa7bb0",
+    "url": "/index.html"
+  }
+];
